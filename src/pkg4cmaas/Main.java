eca68005b9e45c9a3c6b1425553a4ff10c538e22
Efe Özgür <efecuneytozgur@gmail.com>
2017-07-12 22:35:17 +0300

    Ekrana ortala

--- src/pkg4cmaas/Main.java
+++ src/pkg4cmaas/Main.java
@@ -1,14 +1,24 @@
 package pkg4cmaas;
 
+import java.awt.Dimension;
 import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args) {
+        Dimension ekran = Toolkit.getDefaultToolkit().getScreenSize();
+        double yukseklik = ekran.getHeight();
+        double genislik = ekran.getWidth();
+        
         Anaform frm = new Anaform();
         frm.setAlwaysOnTop(true);
         frm.setResizable(false);
-        frm.setVisible(true); 
+        System.out.println(frm.getWidth());
+        System.out.println(frm.getHeight());
+        System.out.println(yukseklik);
+        System.out.println(genislik);
+        frm.setLocation(((int)genislik/2)-(frm.getWidth()/2), ((int) yukseklik/2)-(frm.getHeight()/2));
+        System.out.println(frm.getLocation());
         
-    //    burada kaldım
+        frm.setVisible(true); 
     }
 }
