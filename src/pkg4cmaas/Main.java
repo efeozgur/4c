f25d3431a7d304c09e5a561b164f061fb1ca93fd
Efe Özgür <efecuneytozgur@gmail.com>
2017-07-14 13:29:00 +0300

    Main Güzelleme

--- src/pkg4cmaas/Main.java
+++ src/pkg4cmaas/Main.java
@@ -2,13 +2,13 @@ package pkg4cmaas;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.text.DecimalFormat;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
-import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
     public static void main(String[] args) {
+        /* Burada Nimbus'u aktif yapıyoruz */
         try {
             for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -17,18 +17,15 @@ public class Main {
                 }
             }
         } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
         }
-        
         Dimension ekran = Toolkit.getDefaultToolkit().getScreenSize();
         double yukseklik = ekran.getHeight();
         double genislik = ekran.getWidth();
         Anaform frm = new Anaform();
         frm.setAlwaysOnTop(true);
         frm.setResizable(false);
-        
         frm.setLocation(((int)genislik/2)-(frm.getWidth()/2), ((int) yukseklik/2)-(frm.getHeight()/2));
-
-        
-        frm.setVisible(true); 
+        frm.setVisible(true);         
     }
 }
